7765d40e35c0d371b285da77da7a4949fceb8583
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 13:05:17 +0100

    Mock inputController

---
+++ src/test/java/TQSOthello/MockInputController.java
@@ -0,0 +1,27 @@
+package TQSOthello;
+
+import java.util.ArrayList;
+
+public class MockInputController extends InputController{
+	
+	ArrayList<Tuple<Integer,Integer>> inputList;
+	int index = 0;
+	
+	public MockInputController() {
+		super();
+		inputList = new ArrayList<Tuple<Integer,Integer>>();
+	}
+	
+	public void addInput(Tuple<Integer,Integer> input) {
+		inputList.add(input);
+	}
+	
+	public Tuple<Integer,Integer> getNextInput(){
+		if (index + 1 < inputList.size() ) {
+			index++;
+			return inputList.get(index-1);
+		}
+		return null;
+	}
+	
+}
