1eef1e40da9dca2dae4b349bd59a17a6c4791b0f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 09:48:35 +0100

    Clase Play - Implementación constructor 2

--- src/main/java/TQSOthello/Play.java
+++ src/main/java/TQSOthello/Play.java
@@ -14,6 +14,11 @@ public class Play {
 		}else if (origin.x > 8) {
 			origin.x = 8;
 		}
+		if(origin.y < 0) {
+			origin.y = 0;
+		}else if (origin.y > 8) {
+			origin.y = 8;
+		}
 		ends = new ArrayList<Tuple<Integer, Integer>>();
 	}
 	
