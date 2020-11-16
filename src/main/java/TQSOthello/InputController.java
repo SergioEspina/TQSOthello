fb53b247023b7fe0dbb8110f4ce4fcde8c04df4e
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 11:47:29 +0100

    Clase Game - Implementación inputController

---
+++ src/main/java/TQSOthello/InputController.java
@@ -0,0 +1,52 @@
+package TQSOthello;
+
+import java.util.Scanner;
+
+public class InputController {
+private Scanner s;
+	
+	public InputController() {
+		s = new Scanner(System.in);		
+	}
+	
+	public Tuple<Integer,Integer> askPosition(){
+		Tuple<Integer, Integer> position = new Tuple<Integer,Integer>(-1,-1);
+		
+		int input;
+		
+		do {
+			System.out.print("Row: ");
+			if(s.hasNextInt()) {
+				input = s.nextInt();
+				if (input >= 0 && input <= 8) {
+					position.x = input -1 ;
+					break;
+				}else {
+					System.out.println("The value must be between 1 and 8");
+				}
+			}else {
+				System.out.println("The number must be an integer");
+				s.hasNext();
+			}
+		}while(true);
+		
+		do {
+			System.out.print("Column: ");
+			if(s.hasNextInt()) {
+				input = s.nextInt();
+				if (input >= 0 && input <= 8) {
+					position.y = input -1 ;
+					break;
+				}else {
+					System.out.println("The value must be between 1 and 8");
+				}
+			}else {
+				System.out.println("The number must be an integer");
+				s.hasNext();
+			}
+		}while(true);
+		
+		return position;
+	}
+	
+}
